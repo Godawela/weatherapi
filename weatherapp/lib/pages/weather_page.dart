8a357e3ff27499e541b0878823bad4822be9a4d0
Godawela <godawelaus.21@uom.lk>
2024-09-19 20:45:55 +0530

    change animation according to weather

--- weatherapp/lib/pages/weather_page.dart
+++ weatherapp/lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weatherapp/Models/weathermodel.dart';
 import 'package:weatherapp/services/weather_service.dart';
 
@@ -6,67 +7,94 @@ class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
 
   @override
-  State <WeatherPage> createState() => _WeatherPageState();
+  State<WeatherPage> createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-
-  //api key
-  final _weatherService = WeatherService('bc9e956e963996bca3ef3acc31f6278e'
-);
-
+  // API key
+  final _weatherService = WeatherService('bc9e956e963996bca3ef3acc31f6278e');
   Weathermodel? _weather;
 
-  //fetch weather
+  // Fetch weather
   _fetchWeather() async {
-
-    //get the current city
-      String cityName = await _weatherService.getCurrentCity();
-    //get weather for city
-
-      try{
-        final WeatherModel= await _weatherService.getWeather(cityName);
-
-        setState(() {
-          _weather =WeatherModel;
-        });
-      }
-
-      //any error
-
-      catch(e){
-        print(e);
-      }
+    // Get the current city
+    String cityName = await _weatherService.getCurrentCity();
+    // Get weather for the city
+    try {
+      final WeatherModel = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = WeatherModel;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
-  //weather animations
 
-  //int state
+// weather animations
+String getWeatherAnimation(String? mainCondition) {
+if (mainCondition == null) return 'assets/sunny.json'; // default to sunny
+
+switch (mainCondition. toLowerCase()) {
+  case 'clouds':
+  case 'mist':
+  case 'smoke':
+  case 'haze':
+  case 'dust':
+  case 'fog':
+  case 'few clouds':
+  case 'overcast clouds':
+
+    return 'assets/cloud.json';
+  case 'rain':
+  case 'drizzle':
+  case 'shower rain':
+    return 'assets/rain.json';
+  case 'thunderstorm':
+    return 'assets/thunder.json';
+  case 'clear':
+    return 'assets/sunny.json';
+  default:
+    return 'assets/sunny.json';
+
+}
+}
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-
-    //fetch weather on start up
+    // Fetch weather on start up
     _fetchWeather();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
+     
       body: Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // City name
+            Text(_weather?.cityName ?? "Loading...",
+            style: TextStyle(color: Colors.white,fontSize: 30,),  // Set font color to white
+            
+            ),
+            // Animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),  // Relative path to asset
+            // Temperature
+            Text('${_weather?.temperature.round()}°C',
+            style: TextStyle(color: Colors.white,fontSize: 25,),  // Set font color to white
+            ),
 
-        children: [
-          //city name
-          Text(_weather?.cityName ?? "loading"),
+            //weather condition
 
-          //temp
-          Text('${_weather?.temperature.round()}*C')
-        ],
+            Text(_weather?.mainCondition?? "",
+            style: TextStyle(color: Colors.white),  // Set font color to white
+            ),
+          ],
+        ),
       ),
-    )
     );
   }
 }
